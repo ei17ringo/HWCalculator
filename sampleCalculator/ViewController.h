ea36aafcffe8d85220a948b434d063fc0c081cfe
ei17ringo <ei17ringo@gmail.com>
2015-04-07 16:44:03 +0800

    Initial Commit

---
+++ sampleCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sampleCalculator
+//
+//  Created by Eriko Ichinohe on 2015/04/07.
+//  Copyright (c) 2015年 Eriko Ichinohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

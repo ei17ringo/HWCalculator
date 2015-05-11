8f0c7f48a13cc659de015f0198d2f641d8e7c9fe
ei17ringo <ei17ringo@gmail.com>
2015-05-11 10:45:26 +0800

    init commit

--- sampleCalculator/ViewController.h
+++ sampleCalculator/ViewController.h
@@ -8,8 +8,82 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    int _inputFirtst;
+    int _inputSecond;
+    int _calcCode;
+}
+extern const int ADDCODE;
+extern const int SUBCODE;
+extern const int MULCODE;
+extern const int DIVCODE;
+extern const int NANCODE;
 
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnOne;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnTwo;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnThree;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnFour;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnFive;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnSix;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnSeven;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnEight;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnNine;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnZero;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnMultiply;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnDevide;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnSubtract;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnAdd;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnResult;
+
+- (IBAction)tapOne:(id)sender;
+
+- (IBAction)tapTwo:(id)sender;
+
+- (IBAction)tapThree:(id)sender;
+
+- (IBAction)tapFour:(id)sender;
+
+- (IBAction)tapFive:(id)sender;
+
+- (IBAction)tapSix:(id)sender;
+
+- (IBAction)tapSeven:(id)sender;
+
+- (IBAction)tapEight:(id)sender;
+
+- (IBAction)tapNine:(id)sender;
+
+- (IBAction)tapZero:(id)sender;
+
+- (IBAction)tapDevide:(id)sender;
+
+- (IBAction)tapMultiply:(id)sender;
+
+- (IBAction)tapSubstract:(id)sender;
+
+- (IBAction)tapAdd:(id)sender;
+
+- (IBAction)tapResult:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnClear;
+
+- (IBAction)tapClear:(id)sender;
 
 @end
 
--- sampleCalculator/ViewController.m
+++ sampleCalculator/ViewController.m
@@ -14,9 +14,20 @@
 
 @implementation ViewController
 
+const int ADDCODE = 1;
+const int SUBCODE = 2;
+const int MULCODE = 3;
+const int DIVCODE = 4;
+const int NANCODE = -1;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //初期値代入
+    _inputFirtst = 0;
+    _inputSecond = 0;
+    _calcCode = NANCODE;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +35,146 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)tapOne:(id)sender {
+    int pushNumber = 1;
+    [self tapNumber:pushNumber];
+}
+
+- (IBAction)tapTwo:(id)sender {
+    int pushNumber = 2;
+    [self tapNumber:pushNumber];
+}
+
+- (IBAction)tapThree:(id)sender {
+    int pushNumber = 3;
+    [self tapNumber:pushNumber];
+}
+
+- (IBAction)tapFour:(id)sender {
+    int pushNumber = 4;
+    [self tapNumber:pushNumber];
+}
+
+- (IBAction)tapFive:(id)sender {
+    int pushNumber = 5;
+    [self tapNumber:pushNumber];
+}
+
+- (IBAction)tapSix:(id)sender {
+    int pushNumber = 6;
+    [self tapNumber:pushNumber];
+}
+
+- (IBAction)tapSeven:(id)sender {
+    int pushNumber = 7;
+    [self tapNumber:pushNumber];
+}
+
+- (IBAction)tapEight:(id)sender {
+    int pushNumber = 8;
+    [self tapNumber:pushNumber];
+}
+
+- (IBAction)tapNine:(id)sender {
+    int pushNumber = 9;
+    [self tapNumber:pushNumber];
+}
+
+- (IBAction)tapZero:(id)sender {
+    int pushNumber = 0;
+    [self tapNumber:pushNumber];
+}
+
+- (IBAction)tapDevide:(id)sender {
+    _calcCode = DIVCODE;
+    if (_inputSecond != 0) {
+        _inputFirtst = _inputFirtst / _inputSecond;
+    }
+    _inputSecond = 0;
+    
+    self.resultLabel.text = [NSString stringWithFormat:@"%d",_inputFirtst];
+}
+
+- (IBAction)tapMultiply:(id)sender {
+    _calcCode = MULCODE;
+    if (_inputSecond != 0) {
+        _inputFirtst = _inputFirtst * _inputSecond;
+    }
+    _inputSecond = 0;
+    
+    self.resultLabel.text = [NSString stringWithFormat:@"%d",_inputFirtst];
+}
+
+- (IBAction)tapSubstract:(id)sender {
+    _calcCode = SUBCODE;
+    _inputFirtst = _inputFirtst - _inputSecond;
+    _inputSecond = 0;
+    
+    self.resultLabel.text = [NSString stringWithFormat:@"%d",_inputFirtst];
+}
+
+- (IBAction)tapAdd:(id)sender {
+    _calcCode = ADDCODE;
+    _inputFirtst = _inputFirtst + _inputSecond;
+    _inputSecond = 0;
+    
+    self.resultLabel.text = [NSString stringWithFormat:@"%d",_inputFirtst];
+    
+}
+
+- (IBAction)tapResult:(id)sender {
+    int Result = 0;
+    
+    switch (_calcCode) {
+        case ADDCODE:
+            Result = _inputFirtst + _inputSecond;
+            break;
+        case SUBCODE:
+            Result = _inputFirtst - _inputSecond;
+            break;
+        case MULCODE:
+            Result = _inputFirtst * _inputSecond;
+            break;
+        case DIVCODE:
+            if (_inputSecond == 0) {
+                Result = 0;
+            }else{
+                Result = _inputFirtst / _inputSecond;
+            }
+            break;
+        default:
+            break;
+    }
+    
+    
+    self.resultLabel.text = [NSString stringWithFormat:@"%d",Result];
+    _inputFirtst = Result;
+    _inputSecond = 0;
+    _calcCode = NANCODE;
+}
+
+//数値のボタンが押された時の関数
+- (void)tapNumber:(int)pushedNumber{
+
+    int viewNumber = 0;
+    
+    if (_calcCode == NANCODE) {
+        _inputFirtst = _inputFirtst*10 + pushedNumber;
+        viewNumber = _inputFirtst;
+    }else{
+        _inputSecond = _inputSecond*10 + pushedNumber;
+        viewNumber = _inputSecond;
+    }
+    
+    self.resultLabel.text = [NSString stringWithFormat:@"%d",viewNumber];
+    
+}
+
+- (IBAction)tapClear:(id)sender {
+    //初期値代入
+    _inputFirtst = 0;
+    _inputSecond = 0;
+
+    self.resultLabel.text = @"0";
+}
 @end
